01bbcf8775992f6df9bd539a0f9e51a04a4534ab
18611 <18611688878@163.com>
2025-09-01 20:25:41 +0800

    ?

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 
-int main() {
-    printf("hello world!\n");
+int printboard(int i)
+{
+    system("cls");
+    for (int j = 0; j < 60; j++)
+    {
+
+
+        if (j == i )
+            printf("A");
+
+        else
+            printf(" ");
+
+
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    COORD size = { 60, 50 };
+    char c[60];
+    for (int i = 0; i < 60; i++)
+    {
+        printboard( i );
+        Sleep(30);
+    }
+    for (int i = 0; i < 60; i++)
+    {
+        printboard( 60 - i);
+        Sleep(30);
+    }
     return 0;
 }
