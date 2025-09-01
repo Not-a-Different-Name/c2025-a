c5707f361229b5c4d29ca83409ce790b5854df91
18611 <18611688878@163.com>
2025-09-01 20:43:02 +0800

    ?

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,38 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+int main(void)
+{
+    int n;
+    printf("\n Enter a number: ");
+    scanf("%d", &n);
+    if(n <= 3 && n > 0)
+        printf("%d is  prime\n", n);
+    if(n > 3)
+    {
+        int k = sqrt(n);
+        for(int i = 2; i <= k; i++)
+        {
+            if(n % i == 0)
+            {
+                printf("%d is not prime\n", n);
+                break;
+            }else if (i >= k)
+            {
+                printf("%d is  prime\n", n);
+                break;
+            }
+
+        }
+    system("pause");
+        return 0;
+
+
+
+    }
+
+
+
+
 }
