84cfc7b8e89cb6ce0e22a3122ac0cc19227fb7c4
18611 <18611688878@163.com>
2025-09-01 21:05:27 +0800

    连续质数

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,37 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    printf("hello world!\n");
+int main(void)
+{
+    for (int n=2; n<=1000; n++)
+    {
+    if(n <= 3 && n > 0)
+        printf("%d \n", n);
+    if(n > 3) {
+        int k = sqrt(n);
+        for(int i = 2; i <= k; i++)
+        {
+            if(n % i == 0)
+            {
+                //printf("%d is not prime\n", n);
+                break;
+            }else if (i >= k)
+            {
+                printf("%d \n", n);
+                break;
+            }
+
+        }
+    }
+
+
+
+
+    }
+
+
+
+    system("pause");
     return 0;
 }
