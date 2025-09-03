eb71ee9995455334499e6af1b82fb146ddca370b
18611 <18611688878@163.com>
2025-09-04 00:02:13 +0800

    VS编译可不Sleep(); clion输出结果为0，改加Sleep()后正常输出

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <math.h>
+#include <time.h>
 
 int main(void)
 {
-    for (int n=2; n<=1000; n++)
+    clock_t start = clock(); // 计时开始
+    Sleep(1);
+    for (int n = 2; n <= 1000; n++)
     {
-    if(n <= 3 && n > 0)
-        printf("%d \n", n);
-    if(n > 3) {
-        int k = sqrt(n);
-        for(int i = 2; i <= k; i++)
-        {
-            if(n % i == 0)
+
+        if (n <= 3 && n > 0)
+            printf("%d \n", n);
+        if (n > 3) {
+            int k = sqrt(n);
+            for (int i = 2; i <= k; i++)
             {
-                //printf("%d is not prime\n", n);
-                break;
-            }else if (i >= k)
-            {
-                printf("%d \n", n);
-                break;
+                if (n % i == 0)
+                {
+                    //printf("%d is not prime\n", n);
+                    break;
+                }
+                else if (i >= k)
+                {
+                    printf("%d \n", n);
+                    break;
+                }
+
             }
-
         }
-    }
-
-
 
 
     }
-
-
-
-    system("pause");
+    clock_t end = clock(); // 计时结束
+    double duration = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("运行时间: %.100f 秒\n", duration);
+    /*system("pause");*/
     return 0;
 }
