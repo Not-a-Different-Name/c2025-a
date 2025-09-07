b8f30224045a996489897d2400e03a1abf7484e5
18611 <18611688878@163.com>
2025-09-07 23:39:30 +0800

    finish

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -16,7 +16,7 @@ void hannuo(int n, char pos1, char pos2, char pos3)//1借2移动至3
 {
     if (n == 1) move(pos1, pos3);//最后一层
     else
-    {
+    { 
         hannuo(n - 1, pos1, pos3, pos2);//1移动至2
         move(pos1, pos3);//剩下较大数移动至3
         hannuo(n - 1, pos2, pos1, pos3);//2移动至3
