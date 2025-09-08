59eca66ca58031f3f0c724fef9cb2a8b7287b540
18611 <18611688878@163.com>
2025-09-08 20:13:30 +0800

    异或操作不懂，只能用加减了

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,46 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS 1
 
-int main() {
-    printf("hello world!\n");
+// 加密函数
+void encryption(char* data, int key)
+{
+    for (int i = 0; data[i] != '\0'; i++)
+    {
+        // data[i] = data[i] ^ key ;
+        data[i] = data[i] + key ;
+    }
+    printf("encrypt string: %s\n", data);
+}
+
+// 解密函数
+void decrypt(char* data, int key)
+{
+    for (int i = 0; data[i] != '\0'; i++)
+    {
+        // data[i] = data[i]  ^ key;
+        data[i] = data[i]  - key;
+    }
+    printf("decrypt string: %s\n", data);
+}
+
+int main(void)
+{
+    char data[1024];
+    int key;
+
+    printf("input string: ");
+    scanf_s("%1023s", data, (unsigned)_countof(data));
+
+    printf("input key: ");
+    scanf_s("%d", &key);
+
+    encryption(data, key);
+
+    decrypt(data, key);
+
     return 0;
 }
