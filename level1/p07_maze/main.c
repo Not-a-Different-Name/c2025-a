9d042360c59f3e55f2c42d818ff07fb93aa45c78
18611 <18611688878@163.com>
2025-09-08 21:06:51 +0800

    异或操作不懂，只能用加减了

--- level1/p07_maze/main.c
+++ level1/p07_maze/main.c
@@ -1,6 +1,12 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
 
 int main() {
-    printf("hello world!\n");
+    printf("墙\n");
     return 0;
 }
